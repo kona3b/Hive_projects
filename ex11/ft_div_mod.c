69eb4690c345a0642de4349e9f8458256b98c177
Konsta Aittola <kaittola@c2r3p5.hive.fi>
2021-10-29 11:40:08 +0300

    adding finished ex11

--- ex11/ft_div_mod.c
+++ ex11/ft_div_mod.c
@@ -6,10 +6,12 @@
 /*   By: kaittola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 14:46:25 by kaittola          #+#    #+#             */
-/*   Updated: 2021/10/28 14:57:22 by kaittola         ###   ########.fr       */
+/*   Updated: 2021/10/29 11:36:05 by kaittola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-
+	*div = a / b;
+	*mod = a % b;
+}
